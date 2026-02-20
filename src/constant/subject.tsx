aa97c649a02708d6eaf8010d3f44e7e1dc0b59c8
sudarshan-hash <tsudarshan96@gmail.com>
2026-02-20 11:22:50 +0530

    subject added

--- src/constant/subject.tsx
+++ src/constant/subject.tsx
@@ -1,38 +1,53 @@
 import type { Subject } from "./type";
-import { SiJavascript, SiPython, SiReact, SiTypescript } from "react-icons/si";
+import { SiCss3, SiHtml5, SiJavascript, SiPython, SiReact, SiTypescript } from "react-icons/si";
 import { DiJava } from "react-icons/di";
 
+let id = 0
 const subjects: Subject[] = [
   {
-    id: 1,
+    id: ++id,
+    link: "#",
+    title: "HTML",
+    idTitle: "html",
+    subIcon: <SiHtml5 className="w-5 h-5 text-[#f00808]" />
+  },
+  {
+    id: ++id,
+    link: "#",
+    title: "CSS",
+    idTitle: "css",
+    subIcon: <SiCss3 className="w-5 h-5 text-[#8335f7]" />
+  },
+  {
+    id: ++id,
     link: "#",
     title: "Java Script",
     idTitle: "js",
     subIcon: <SiJavascript className="w-5 h-5 text-[#cfd907]" />
   },
   {
-    id: 2,
+    id: ++id,
     link: "#",
     title: "Type Script",
     idTitle: "ts",
     subIcon: <SiTypescript className="w-5 h-5 text-[#1b07cf]" />,
   },
   {
-    id: 3,
+    id: ++id,
     link: "#",
     title: "React",
     idTitle: "react",
     subIcon: <SiReact className="w-5 h-5 text-[#0e07d9]" />,
   },
   {
-    id: 4,
+    id: ++id,
     link: "#",
     title: "Python",
     idTitle: "python",
     subIcon: <SiPython className="w-5 h-5 text-[#6907d9]" />,
   },
   {
-    id: 5,
+    id: ++id,
     link: "#",
     title: "Java",
     idTitle: "java",
